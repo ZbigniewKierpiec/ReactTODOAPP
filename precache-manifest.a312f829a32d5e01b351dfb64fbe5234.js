43989d1b4e1c145921508a5e66c02af0afbf0ce9
Zbigniew Kierpiec <zbigniewkierpiec@gmail.com>
2020-06-18 10:58:44 +0100

    Updates

---
+++ precache-manifest.a312f829a32d5e01b351dfb64fbe5234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc13b6ff4b4be527b2921e075c1bcd0",
+    "url": "/ReactToDoApp/index.html"
+  },
+  {
+    "revision": "bd4bdebe30805e72b995",
+    "url": "/ReactToDoApp/static/css/main.cae39fb4.chunk.css"
+  },
+  {
+    "revision": "badbc9914ac4d779a874",
+    "url": "/ReactToDoApp/static/js/2.8167cb57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactToDoApp/static/js/2.8167cb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd4bdebe30805e72b995",
+    "url": "/ReactToDoApp/static/js/main.7edd2238.chunk.js"
+  },
+  {
+    "revision": "3fd45417871519432995",
+    "url": "/ReactToDoApp/static/js/runtime-main.1ff20395.js"
+  }
+]);
